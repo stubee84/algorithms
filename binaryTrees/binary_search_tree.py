e6966d5f6cddd8ab125d4cca158576b98f0853b0
sbrannon <stuart.brannon@sprint.com>
2020-11-13 17:26:27 -0500

    finished remove() and worked on balance()

--- binaryTrees/binary_search_tree.py
+++ binaryTrees/binary_search_tree.py
@@ -2,7 +2,6 @@ class Tree:
     def __init__(self):
         self.left_node = None
         self.right_node = None
-        self.parent = None
         self.tree = None
 
 class BST(Tree):
@@ -52,10 +51,35 @@ class BST(Tree):
         else:
             return None
 
-    def balance(self):
-        pass
+    def balance(self, init_num: int = None):
+        if init_num is None:
+            if self.left_node is not None:
+                num = self.left_node.balance()
+
+                if num > self.root:
+                    self.balance(init_num=num)
+            if self.right_node is not None:
+                num = self.right_node.balance()
+
+                if num < self.root:
+                    self.balance(init_num=num)
+        else:
+            if init_num < self.root:
+                if self.left_node is not None:
+                    num = self.left_node.balance(init_num=init_num)
+                else:
+                    self.left_node.root = init_num
+                    self.right_node = self.right_node.right_node
+            elif init_num > self.root:
+                if self.right_node is not None:
+                    num = self.right_node.balance(init_num=init_num)
+                else:
+                    self.right_node.root = init_num
+                    self.left_node = self.left_node.left_node
+        return self.root
 
     def print(self):
+        self.balance()
         print(self.tree)
 
 def build_tree(numbers: list) -> BST:
@@ -70,18 +94,4 @@ def build_tree(numbers: list) -> BST:
 if __name__ == "__main__":
     numbers = [4,2,9,10,1,0,8,13]
 
-    build_tree(numbers=numbers)
-
-
-    '''
-                    8
-
-        4               10
-
-    1        5       9       13
-
-0       2
-add 5
-
-
-    '''
+    bst = build_tree(numbers=numbers)
